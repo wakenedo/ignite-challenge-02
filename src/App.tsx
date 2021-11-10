e4728494dcbb7242e84bc4bea2cadf6a31dab141
wakenedo <a_nuner@outlook.com>
2021-11-10 15:34:35 -0300

    Part2 - Content

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
 
 import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -19,7 +19,7 @@ export interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
+export interface MovieProps {
   imdbID: string;
   Title: string;
   Poster: string;
@@ -64,19 +64,8 @@ export function App() {
       selectedGenreId={selectedGenreId}
       />
 
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content movies={movies} selectedGenre={selectedGenre} 
+      />
     </div>
   )
 }
